bdafd26baf711ac8c62275b2350639dc4c91337d
Colin Moore <colinmoore.dev@gmail.com>
2015-03-30 15:34:35 -0700

    Added Elect controller

---
+++ app/scripts/controllers/elect.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stvApp.controller:ElectCtrl
+ * @description
+ * # ElectCtrl
+ * Controller of the stvApp
+ */
+angular.module('stvApp')
+  .controller('ElectCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
