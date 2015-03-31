36d9a2297996063cb33a745b8433299cbf4d9406
Colin Moore <colinmoore.dev@gmail.com>
2015-03-31 11:11:32 -0700

    setup first pass of candidate data binding with elect ctrl - data binding is not yet connected to factory creation for candidates

--- app/scripts/controllers/elect.js
+++ app/scripts/controllers/elect.js
@@ -8,10 +8,16 @@
  * Controller of the stvApp
  */
 angular.module('stvApp')
-  .controller('ElectCtrl', function ($scope) {
+  .controller('ElectCtrl', function ($scope, Election) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+	$scope.candidatesArray = [{firstname:'sdgds', lastname:'lastafdg'}, {firstname:'sdg', lastname:'lastagdfs'}];
+    $scope.election = new Election($scope.candidatesArray);
+
   });
