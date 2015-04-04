cad03cb8c9b194730d9eaf5c60af29064a49ec33
Colin Moore <colinmoore.dev@gmail.com>
2015-04-04 15:57:19 -0700

    Store the raw vote data for each vote in election.votesArray votePrefs now stores references in to election.votesArray rather than just tallys of votes, this enables votePrefs to determine secondary preferences when transferring votes.
    The elect view now displays a list of all the votes.

--- app/scripts/services/election.js
+++ app/scripts/services/election.js
@@ -51,20 +51,35 @@ angular.module('stvApp')
       this.placeVotes();
     };
 
+    this.getCandidate = function( candidateKey) {
+
+      var candidateWithKey = 0;
+      angular.forEach(this.candidatesArray, ( function(thisCandidate) {
+        if (thisCandidate.key === candidateKey) {
+          candidateWithKey = thisCandidate;
+        }
+      }).bind(this)); 
+      return candidateWithKey;
+    };
+
     this.placeVotes  = function() {
      
 
       // Reset votes...
 
+      // initialise votes data store
+      this.votesArray = [];
+
       //initilaize vote pref data store
       this.votePref = []; // index 0: vote pref; index 1: hash map of key of candidate; value: tally of votes for combination of vote pref and candidate.
+
       angular.forEach(this.candidatesArray, ( function() {
           this.votePref.push( {} );
         }).bind(this)); 
       
       angular.forEach(this.candidatesArray, ( function(thisCandidate) {
         for ( var votePreferenceIndex=0; votePreferenceIndex < this.candidatesArray.length; votePreferenceIndex++) {
-          this.votePref[votePreferenceIndex][thisCandidate.key] = 0;
+          this.votePref[votePreferenceIndex][thisCandidate.key] = [];
         }
        }).bind(this)); 
 
@@ -73,13 +88,23 @@ angular.module('stvApp')
         // for this voter, randomly generate ranking of available candidates.
         var voterOptionsArray = this.candidatesArray.slice();
 
+        var thisVote = [];
+
         for ( var votePreferenceIndex=0; votePreferenceIndex < this.candidatesArray.length; votePreferenceIndex++) {
             var randomCandidateIndex = Math.floor(Math.random() * voterOptionsArray.length);
             var candidateKey = voterOptionsArray[randomCandidateIndex].key ;
-            this.votePref[ votePreferenceIndex ][ candidateKey ] += 1;
+            thisVote.push(candidateKey);
             voterOptionsArray.splice(randomCandidateIndex, 1);
         }
+        this.votesArray.push( thisVote); 
       }
+
+      // build votePref mapping table from votes
+      this.votesArray.forEach( function(thisVote) {
+        thisVote.forEach( function(candidateKey, preferenceIndex) {
+            this.votePref[ preferenceIndex ][ candidateKey ].push(thisVote);
+          }.bind(this));
+        }.bind(this));
 
       // results resolution
       this.voteResolutionRounds = [];
@@ -93,6 +118,8 @@ angular.module('stvApp')
         // create new round
         var thisVotingRound = new VotingRound( latestVotingRound.votePref );
         this.voteResolutionRounds.push(thisVotingRound);
+
+        // transfer any surplus votes to other candidates
 
         break; // for now only handle up to one additonal round
       }
@@ -109,7 +136,7 @@ angular.module('stvApp')
     this.voteResolutionConditionsMet = function ( ) {
 
       var quotaMet = this.candidatesArray.some( function( thisCandidate ) { 
-          if ( this.votePref[0][thisCandidate.key] >= this.getDroopQuota()) {
+          if ( this.votePref[0][thisCandidate.key].length >= this.getDroopQuota()) {
             return true;
           }
           return false;
